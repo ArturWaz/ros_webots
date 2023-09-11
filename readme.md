5d7f36a97c8bff9eb8111254d245c49a1274d9c6
aw <aw@bc.com>
2023-09-11 18:01:34 +1000

    basic example of webots robot control

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 Basic setup
 ===
 
+```bash
+xhost +
+```
+
+Source example in here: <https://docs.ros.org/en/iron/Tutorials/Advanced/Simulators/Webots/Setting-Up-Simulation-Webots-Advanced.html>
--- src/my_package/launch/robot_launch.py
+++ src/my_package/launch/robot_launch.py
@@ -1,6 +1,7 @@
 import os
 import launch
 from launch import LaunchDescription
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
@@ -18,12 +19,18 @@ def generate_launch_description():
         robot_name='my_robot',
         parameters=[
             {'robot_description': robot_description_path},
-        ],
+        ]
+    )
+
+    obstacle_avoider = Node(
+        package='my_package',
+        executable='obstacle_avoider',
     )
 
     return LaunchDescription([
         webots,
         my_robot_driver,
+        obstacle_avoider,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
